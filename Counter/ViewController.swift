aaea2ebc52adf032236716680978b3acaa0458c6
Elizabeth <lmazulova@bk.ru>
2024-09-13 15:29:20 +0300

    Дз сделано

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -4,16 +4,44 @@
 //
 //  Created by user on 12.09.2024.
 //
-
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var plus: UIButton!
+    @IBOutlet weak var minus: UIButton!
+    @IBOutlet weak var restart: UIButton!
+    @IBOutlet weak var history: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func add(_ sender: Any) {
+        let score = Int(number.text!)!
+        number.text = String(score + 1)
+        let currentDate = Date().formatted(date: .numeric, time: .standard)
+        history.text += "\n\(currentDate): значение изменено на +1"
+    }
+    
+    @IBAction func subtraction(_ sender: Any) {
+        let score = Int(number.text!)!
+        let currentDate = Date().formatted(date: .numeric, time: .standard)
+        if score > 0 {
+            number.text = String(score - 1)
+            history.text += "\n\(currentDate): значение изменено на -1"
+        } else {
+            
+            history.text += "\n\(currentDate): попытка уменьшить значение счетчика ниже 0"
+        }
+    }
+    
 
-
+    @IBAction func changeToZero(_ sender: Any) {
+        number.text = "0"
+        let currentDate = Date().formatted(date: .numeric, time: .standard)
+        history.text += "\n\(currentDate): значение сброшено"
+    }
 }
-
