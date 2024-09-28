518bfde9953ed93232bc96165fa11ab4fc0b9e07
Elizabeth <lmazulova@bk.ru>
2024-09-28 19:59:13 +0300

    Все исправила, теперь точно

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,39 +8,34 @@ import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var plus: UIButton!
-    @IBOutlet weak var minus: UIButton!
-    @IBOutlet weak var restart: UIButton!
-    @IBOutlet weak var history: UITextView!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet weak var restartButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
-    @IBAction func add(_ sender: Any) {
-        let score = Int(number.text!)!
-        number.text = String(score + 1)
+    @IBAction func add(_ sender: UIButton) {
+        let score = Int(numberLabel.text!)!
+        numberLabel.text = String(score + 1)
         let currentDate = Date().formatted(date: .numeric, time: .standard)
-        history.text += "\n\(currentDate): значение изменено на +1"
+        historyTextView.text += "\n\(currentDate): значение изменено на +1"
     }
-    
-    @IBAction func subtraction(_ sender: Any) {
-        let score = Int(number.text!)!
+    @IBAction func subtraction(_ sender: UIButton) {
+        let score = Int(numberLabel.text!)!
         let currentDate = Date().formatted(date: .numeric, time: .standard)
         if score > 0 {
-            number.text = String(score - 1)
-            history.text += "\n\(currentDate): значение изменено на -1"
+            numberLabel.text = String(score - 1)
+            historyTextView.text += "\n\(currentDate): значение изменено на -1"
         } else {
-            
-            history.text += "\n\(currentDate): попытка уменьшить значение счетчика ниже 0"
+            historyTextView.text += "\n\(currentDate): попытка уменьшить значение счетчика ниже 0"
         }
     }
-    
-
-    @IBAction func changeToZero(_ sender: Any) {
-        number.text = "0"
+    @IBAction func restart(_ sender: UIButton) {
+        numberLabel.text = "0"
         let currentDate = Date().formatted(date: .numeric, time: .standard)
-        history.text += "\n\(currentDate): значение сброшено"
+        historyTextView.text += "\n\(currentDate): значение сброшено"
     }
 }
